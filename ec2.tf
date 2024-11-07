98a3ebe3468e369d16b6dc89a1efe78321a3b1cc
Gwei Michael Wawa <101484680+Michaelgwei86@users.noreply.github.com>
2024-11-07 19:25:54 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,11 @@
 resource "aws_instance" "web" {
   ami           =var.ami_id
   instance_type = var.instance_type
+  monitoring = true
+  ebs_optimized = true
+  root_block_device {
+  encrypted = true
+  }
 
   tags = {
     Name = "airbnb-web-server"
